5b00184b0f0cdc059d86aac79b4ddba3467f80a6
Jan de Visser <jan@de-visser.net>
2023-10-02 12:47:30 -0400

    Porting to aarch64 on Raspberry Pi
    There is no clang 15 on Raspberry Pi (yet) so we have to compile this lark
    using gcc 12.
    
    - Introduce a 'noreturn' macro which uses the correct incantation for clang
      or gcc.
    - gcc doesn't like '__FILE_NAME__'. '__FILE__' is standard so use that even
      though it includes the path.

--- src/log.h
+++ src/log.h
@@ -11,21 +11,27 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
-extern void              log_init(bool trace_on);
-extern void              trace(char const *msg, ...);
-extern void              vtrace(char const *msg, va_list args);
-[[noreturn]] extern void fatal(char const *msg, ...);
-[[noreturn]] extern void vfatal(char const *msg, va_list args);
+#ifdef __clang__
+#define noreturn [[noreturn]]
+#else
+#define noreturn __attribute__((noreturn))
+#endif
 
-#define UNREACHABLE() fatal("%s:%d: Unreachable", __FILE_NAME__, __LINE__)
-#define NYI(what, ...) fatal("%s:%d: Not yet implemented in %s: " what, __FILE_NAME__, __LINE__, __func__ __VA_OPT__(, ) __VA_ARGS__)
-#define OUT_OF_MEMORY(where) fatal("Out of memory at %s:%d: %s", __FILE_NAME__, __LINE__, where)
+extern void          log_init(bool trace_on);
+extern void          trace(char const *msg, ...);
+extern void          vtrace(char const *msg, va_list args);
+noreturn extern void fatal(char const *msg, ...);
+noreturn extern void vfatal(char const *msg, va_list args);
+
+#define UNREACHABLE() fatal("%s:%d: Unreachable", __FILE__, __LINE__)
+#define NYI(what, ...) fatal("%s:%d: Not yet implemented in %s: " what, __FILE__, __LINE__, __func__ __VA_OPT__(, ) __VA_ARGS__)
+#define OUT_OF_MEMORY(where) fatal("Out of memory at %s:%d: %s", __FILE__, __LINE__, where)
 
 #define assert(cond) \
     if (!(cond))     \
-    fatal(__FILE_NAME__ ":%d: assert(" #cond ") FAILED", __LINE__, #cond)
+    fatal(__FILE__ ":%d: assert(" #cond ") FAILED", __LINE__, #cond)
 #define assert_msg(cond, msg, ...) \
     if (!(cond))                   \
-    fatal(__FILE_NAME__ ":%d: assert(" #cond ") " msg, __LINE__ __VA_OPT__(, ) __VA_ARGS__)
+    fatal(__FILE__ ":%d: assert(" #cond ") " msg, __LINE__ __VA_OPT__(, ) __VA_ARGS__)
 
 #endif // __LOG_H__
